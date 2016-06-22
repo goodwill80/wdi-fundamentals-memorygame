827c9f7a44ed4569cf441feb09ecef1ac85a60c4
Ho Kit Jonathan <jonathan@Hos-MacBook-Air.local>
2016-06-23 02:41:45 +0800

    assignment 9

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,12 @@ var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "Queen";
 
+
+var userChoice = function(a,b){
+if (a === b) {
+	alert("You found a match!");
+} else { alert("Sorry, try again.");
+}
+};
+
+console.log(userChoice(cardOne, cardTwo));
